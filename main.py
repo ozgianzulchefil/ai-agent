5e9efcfc8c3369c0ef9520c724181310d15c2daf
whitenight09 <ozzy@mail.com>
2025-08-25 21:47:10 +0300

    added python usage with arguments

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+if len(sys.argv) < 2: 
+  print("Usage: uv run main.py <your-query-here> ") 
+  sys.exit(1)
+
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.0-flash-001', contents=sys.argv[1]
 )
 print(response.text)
 print(f"Prompt tokens:",response.usage_metadata.prompt_token_count)
