170069d5d8922154c18303ec67a25b33629c7eeb
whitenight09 <ozzy@mail.com>
2025-08-26 19:49:59 +0300

    modified according to solution

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
-import os, sys
-from dotenv import load_dotenv
+import sys
+import os
 from google import genai
+from google.genai import types
+from dotenv import load_dotenv
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-if len(sys.argv) < 2: 
-  print("Usage: uv run main.py <your-query-here> ") 
-  sys.exit(1)
-
-client = genai.Client(api_key=api_key)
-
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=sys.argv[1]
-)
-print(response.text)
-print(f"Prompt tokens:",response.usage_metadata.prompt_token_count)
-print(f"Response tokens:", response.usage_metadata.candidates_token_count)
 
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
+
 
 if __name__ == "__main__":
     main()
