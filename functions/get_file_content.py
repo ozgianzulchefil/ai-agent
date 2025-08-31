a7f204cf76d3e4ad84816ed5783c83cac01896ee
whitenight09 <ozzy@mail.com>
2025-08-31 18:34:37 +0300

    added new function

---
+++ functions/get_file_content.py
@@ -0,0 +1,22 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    wd_abs = os.path.abspath(working_directory)
+    target_abs = os.path.abspath(os.path.join(working_directory, file_path))
+    
+    if not target_abs.startswith(wd_abs):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(target_abs):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    try:
+        with open(target_abs, "r") as f:
+            content = f.read(MAX_CHARS + 1)
+            if len(content) > MAX_CHARS:
+                content = content[:MAX_CHARS] + f'[...File "{file_path}" truncated at 10000 characters]'
+        return content
+            
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,18 +1,26 @@
 # python
 import os, sys
-print("CWD:", os.getcwd())
-print("ROOT CONTENTS:", os.listdir("."))
-print("FUNCTIONS CONTENTS:", os.listdir("functions") if os.path.isdir("functions") else "functions dir not found")
-sys.path.insert(0, os.path.abspath("."))
+# print("CWD:", os.getcwd())
+# print("ROOT CONTENTS:", os.listdir("."))
+# print("FUNCTIONS CONTENTS:", os.listdir("functions") if os.path.isdir("functions") else "functions dir not found")
+# sys.path.insert(0, os.path.abspath("."))
 
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 if __name__ == "__main__":
-    print("Result for current directory:")
-    print(get_files_info("calculator", "."))
-    print("\nResult for 'pkg' directory:")
-    print(get_files_info("calculator", "pkg"))
-    print("\nResult for 'bin' directory:")
-    print(get_files_info("calculator", "/bin"))
-    print("\nResult for '../' directory:")
-    print(get_files_info("calculator", "../"))
+    # below are tests for get_file_content
+    # print("Result for current directory:")
+    # print(get_files_info("calculator", "."))
+    # print("\nResult for 'pkg' directory:")
+    # print(get_files_info("calculator", "pkg"))
+    # print("\nResult for 'bin' directory:")
+    # print(get_files_info("calculator", "/bin"))
+    # print("\nResult for '../' directory:")
+    # print(get_files_info("calculator", "../"))
+
+    # below are tests for get_file_content
+    print(get_file_content("calculator", "main.py"))
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print(get_file_content("calculator", "/bin/cat"))
+    print(get_file_content("calculator", "pkg/does_not_exist.py"))
